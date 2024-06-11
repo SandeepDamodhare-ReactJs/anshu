a9e0540e01daf3016dbdfbc407e2bb50647d10b6
SANDEEPDAMODHARE <SDAMODHARE5@GMAIL.COM>
2024-06-11 17:44:27 +0530

    Mandar

---
+++ src/components/QuestionBank.jsx
@@ -0,0 +1,35 @@
+
+const qBank = [
+	{
+		id: 1,
+		question: "Mandar is from where?",
+		options: ["Harayana", "Amravati", "Nagpur", "Chandigarh"],
+		answer: "Amravati",
+	},
+	{
+		id: 2,
+		question: "Which Car Mandar likes?",
+		options: ["Alto", "Maruti", "Mercedes", "Rolls Royal"],
+		answer: "Rolls Royal",
+	},
+	{
+		id: 3,
+		question: "what is Mandar's favorate Food ?",
+		options: ["Fish", "Chicken", "Mutton", "Pronze"],
+		answer: "Chicken"
+	},
+	{
+		id: 4,
+		question: "Mandar is in Love with?",
+		options: ["Mom", "Papa", "Aai", "Baba"],
+		answer: "Aai"
+	},
+	{
+		id: 5,
+		question: "Which game Rahul Likes?",
+		options: ["BGMI", "Free-Fire", "Cricket", "Foorball"],
+		answer: "BGMI"
+	},
+]
+
+export default qBank;
